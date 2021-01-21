ebce55635345c13d7bcf7a5b1d50ad7ec8a44477
julioenred <julioperezdecastro@gmail.com>
2021-01-21 15:18:14 +0100

    add connection db

--- public/main.js
+++ public/main.js
@@ -1,13 +1,29 @@
+// var socket = io.connect('http://ec2-18-194-88-69.eu-central-1.compute.amazonaws.com', {
+//     'foreceNew': true
+// });
+
 var socket = io.connect('http://localhost:8888', {
     'foreceNew': true
 });
 
-socket.on('messages', function (data) {
-    console.log(data);
-    render(data);
+
+socket.on('users', function (data) {
+    render_users(data);
 });
 
-function render(data) {
+socket.on('contacts', function (data) {
+    render_contacts(data);
+});
+
+socket.on('conversation', function (data) {
+    render_messages(data);
+});
+
+socket.on('groups', function (data) {
+    render_groups(data);
+});
+
+function render_messages(data) {
     var html = data.map(function (elem, index) {
         return (
             `<div>
@@ -17,16 +33,106 @@ function render(data) {
         )
     }).join(" ");
 
+    document.getElementById('conversation').innerHTML = html;
+}
 
-    document.getElementById('messages').innerHTML = html;
+function render_groups(data) {
+    var html = data.map(function (elem, index) {
+        return (
+            `<div id=group-${elem.id} class='group' onclick="set(${elem.id}, '${elem.name}', 'group')">
+                <strong>Grupo</strong>:
+                <em>${elem.name}</em>
+            </div>`
+        )
+    }).join(" ");
+
+    document.getElementById('groups').innerHTML = html;
+}
+
+function render_users(data) {
+    var html = data.map(function (elem, index) {
+        return (
+            `<div id=user-${elem.id} class='user' onclick="set(${elem.id}, '${elem.name}', 'user')">
+                <strong>User</strong>:
+                <em>${elem.name}</em>
+            </div>`
+        )
+    }).join(" ");
+
+    document.getElementById('users').innerHTML = html;
+}
+
+function render_contacts(data) {
+    var html = data.map(function (elem, index) {
+        return (
+            `<div id=contact-${elem.id} class='contact' onclick="set(${elem.id}, '${elem.name}', 'contact')">
+                <strong>Contact</strong>:
+                <em>${elem.name}</em>
+            </div>`
+        )
+    }).join(" ");
+
+    document.getElementById('contacts').innerHTML = html;
+}
+
+function addGroup(e) {
+    var payload = {
+        groupname: document.getElementById('groupname').value,
+    };
+
+    socket.emit('new-group', payload);
+    return false;
 }
 
 function addMessage(e) {
+    author_id = localStorage.getItem('user');
+    author = localStorage.getItem('username');
+    text = document.getElementById('text').value;
+    group_id = localStorage.getItem('group');
+    contact_id = localStorage.getItem('contact');
+
+    if (contact_id != undefined && group_id != undefined) {
+        group_id = undefined;
+    }
+
     var payload = {
-        author: document.getElementById('username').value,
-        text: document.getElementById('texto').value
+        author_id: author_id,
+        author: author,
+        text: text,
+        group_id: group_id,
+        contact_id: contact_id
     };
 
     socket.emit('new-message', payload);
     return false;
+}
+
+function setConversation() {
+    var payload = {
+        author_id: localStorage.getItem('user'),
+        contact_id: localStorage.getItem('contact'),
+        group_id: localStorage.getItem('group')
+    };
+
+    socket.emit('show-conversation', payload);
+    return false;
+}
+
+function set(id, name, type_user) {
+    localStorage.setItem(type_user, id);
+    localStorage.setItem(type_user + 'name', name);
+
+    var els = document.querySelectorAll('.' + type_user)
+    for (var i = 0; i < els.length; i++) {
+        els[i].classList.remove('active')
+    }
+
+    var element = document.getElementById(`${type_user}-${id}`);
+    element.classList.add("active");
+
+    setConversation();
+}
+
+function clean_local_storage() {
+    localStorage.clear();
 }
--- server/main.js
+++ server/main.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var messages = [{
-    id: 1,
-    text: 'hello world',
-    author: 'Julioenred'
-}];
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "chat"
+});
 
 app.use(express.static('public'));
 
@@ -18,14 +20,92 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     console.log('alguien se conecto con sockets');
-    socket.emit('messages', messages);
+    con.connect(function (err) {
+        con.query("SELECT * FROM users", function (err, result, fields) {
+            socket.emit('users', result);
+        });
+
+        con.query("SELECT * FROM users", function (err, result, fields) {
+            socket.emit('contacts', result);
+        });
+
+        // async_get_messages();
+
+        con.query("SELECT * FROM groups", function (err, result, fields) {
+            socket.emit('groups', result);
+        });
+    });
+
+    socket.on('show-conversation', function (data) {
+        async_get_messages(data, io);
+    });
 
     socket.on('new-message', function (data) {
-        messages.push(data);
+        insert_message(data);
+        setTimeout(function () { async_get_messages(data, io); }, 100);
+    });
 
-        io.sockets.emit('messages', messages);
+    socket.on('new-group', function (data) {
+        // messages.push(data);
+        insert_group(data);
+        con.connect(function (err) {
+            con.query("SELECT * FROM groups", function (err, result, fields) {
+                io.sockets.emit('groups', result);
+            });
+        });
     });
 });
+
+function insert_message(message) {
+    con.connect(function (err) {
+        var sql = `INSERT INTO messages (author_id, author, text, group_id, contact_id) 
+                    VALUES (${message.author_id}, '${message.author}', '${message.text}', ${message.group_id}, ${message.contact_id})`;
+        con.query(sql, function (err, result) {
+            console.log(err);
+            console.log("1 record inserted");
+        });
+    });
+}
+
+function insert_group(group) {
+    con.connect(function (err) {
+        var sql = `INSERT INTO groups (name) VALUES ('${group.groupname}')`;
+        con.query(sql, function (err, result) {
+            console.log("1 record inserted");
+        });
+    });
+}
+
+async function async_get_messages(data, io) {
+    try {
+        console.log('data en async');
+        console.log(data);
+        const messages = await get_messages(data);
+        io.sockets.emit('conversation', messages);
+    } catch (error) {
+        console.log('trace');
+        console.log(error.message);
+    }
+}
+
+function get_messages(data) {
+    if (data.contact_id != null) {
+        var query = `SELECT * 
+                    FROM messages 
+                    where (author_id=${data.author_id} and contact_id=${data.contact_id})
+                    or (author_id=${data.contact_id} and contact_id=${data.author_id})`;
+    } else {
+        var query = `SELECT * FROM messages where group_id=${data.group_id}`;
+    }
+
+    return new Promise((resolve, reject) => {
+        con.query(query, function (err, result, fields) {
+            resolve(result);
+        });
+    });
+}
+
+
 
 server.listen(process.env.PORT, function () {
     console.log("Listen on: " + process.env.PORT);
