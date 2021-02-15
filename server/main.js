20ebd687747e66fe92657b4850aa8a7e529f8605
julioenred <julioperezdecastro@gmail.com>
2021-02-15 21:15:31 +0100

    bug from user

--- server/main.js
+++ server/main.js
@@ -465,9 +465,9 @@ async function get_messages_query(data) {
         con.query(messages_sql, function (err, messages, fields) {
             messages.map(function (message, index) {
                 if (message.user_id == data.user_id_request) {
-                    message.from_user = true
+                    message.from_user = false
                 } else {
-                    message.from_user = false;
+                    message.from_user = true;
                 }
 
                 messages[index] = message;
