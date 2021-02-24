6997a40601ef57a2ee48a513f377069b2ab6aa4b
julioenred <julioperezdecastro@gmail.com>
2021-02-24 22:14:22 +0100

    bug - add is read in create group

--- server/main.js
+++ server/main.js
@@ -284,13 +284,25 @@ function insert_group(group) {
                 conversations_id_added = [];
                 for (let i = 0; i < conversations.length; i++) {
                     if (i == 0) {
-                        conversations[i].is_read = is_read.get(conversations[i].conversation_id);
+                        console.log('is_read status >>');
+                        console.log(is_read);
+                        if (is_read.size == 0) {
+                            conversations[i].is_read = 1;
+                        } else {
+                            conversations[i].is_read = is_read.get(conversations[i].conversation_id);
+                        }
                         conversations_formatted.push(conversations[i]);
                         conversations_id_added.push(conversations[i].conversation_id);
                     }
 
                     if ((i + 1 < conversations.length) && !conversations_id_added.includes(conversations[i + 1].conversation_id)) {
-                        conversations[i + 1].is_read = is_read.get(conversations[i + 1].conversation_id);
+                        console.log('is_read status >>');
+                        console.log(is_read);
+                        if (is_read.size == 0) {
+                            conversations[i + 1].is_read = 1;
+                        } else {
+                            conversations[i + 1].is_read = is_read.get(conversations[i + 1].conversation_id);
+                        }
                         conversations_formatted.push(conversations[i + 1]);
                         conversations_id_added.push(conversations[i + 1].conversation_id);
                     }
