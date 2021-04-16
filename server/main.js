a5b5c684f34c3a4932a11b44c4225e646e558fa6
julioenred <julioperezdecastro@gmail.com>
2021-04-16 10:27:27 +0200

    add charset utf8mb4 in node conection mysql

--- server/main.js
+++ server/main.js
@@ -13,7 +13,8 @@ var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    charset: 'utf8mb4'
 });
 
 var storage = multer.diskStorage({
